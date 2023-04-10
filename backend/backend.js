31f29d5e0e93cc9f4f593039c00914bce5c82158
MichalGolan <michalgolan0107@gmail.com>
2023-04-10 20:06:11 +0300

    front get users from db

--- backend/backend.js
+++ backend/backend.js
@@ -1,10 +1,13 @@
 const { PrismaClient } = require("@prisma/client");
 const express = require("express");
+const cors = require("cors");
+
 const prisma = new PrismaClient();
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.post("/user", async (req, res) => {
   const user = await prisma.user.create({ data: req.body });
@@ -12,9 +15,20 @@ app.post("/user", async (req, res) => {
 });
 
 app.get("/user", async (req, res) => {
-  const user = await prisma.user.findFirst();
+  const user = await prisma.user.findFirst(
+    {where:{
+    id: req.session.userId
+  }}
+  );
   res.json(user);
 });
+
+
+app.get("/all-users", async (req, res) => {
+  const users = await prisma.user.findMany()
+  res.json(users);
+});
+
 
 const port = process.env.PORT || "3000";
 
