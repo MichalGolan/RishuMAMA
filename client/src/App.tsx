e624640dedd3910e5e7bd6c7a7bc34af5c7ef89f
michalAronova <michalaronova@gmail.com>
2023-04-28 09:27:32 +0300

    to ts and clean

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+import React from "react";
+import {UsersList} from "./components/UsersList";
+
+export function App() {
+  return (
+    <div className="App">
+      <UsersList />
+    </div>
+  )
+}
