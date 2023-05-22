3d66d7ec4c2a563b6c3c12b1f1d026b717814bd0
MichalGolan <michalgolan0107@gmail.com>
2023-05-22 17:20:51 +0300

    app bar added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css'
 import React from "react";
 import {UsersList} from "./components/UsersList";
-import SidePanel from './components/side panel/SidePanel';
+import SidePanel from './components/side-panel/SidePanel';
+import ResponsiveAppBar from './components/app-bar/App-bar';
 
 export function App() {
   return (
     <div className="App">
+      <ResponsiveAppBar />
       <SidePanel />
     </div>
   )
