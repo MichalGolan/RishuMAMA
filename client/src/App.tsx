2bb985d4e48c61fac2890722cd33892e51e15388
michalAronova <michalaronova@gmail.com>
2023-05-22 17:20:11 +0300

    week planner initial, demo

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,25 @@
 import './App.css'
-import React from "react";
-import {UsersList} from "./components/UsersList";
+import React, {useRef} from "react";
 import SidePanel from './components/side panel/SidePanel';
-
+import {WeeklyPlanner} from "./components/week-plan/WeeklyPlanner";
+import {Container, Row, Col} from "../node_modules/react-bootstrap/esm/index";
 export function App() {
-  return (
-    <div className="App">
-      <SidePanel />
-    </div>
-  )
+    const ref = useRef(null);
+    const temp = "John";
+    return (
+        <div className="App" >
+            <Container>
+                <Row>
+                    <Col sm={6}>
+                         <div style={{ width: 700, height: 400, position: 'relative' }} ref={ref}>
+                            <WeeklyPlanner name={temp} parentRef={ref} />
+                        </div>
+                    </Col>
+                    <Col sm={6}>
+                        <SidePanel />
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+    )
 }
