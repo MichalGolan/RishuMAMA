ec3b9a47c9b280e75ab626eb0a9a688d53857576
michalAronova <michalaronova@gmail.com>
2023-07-19 13:08:42 +0300

    MUI navbar and side menu (demo)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,6 @@
 import React, {useRef} from "react";
-import {Button} from "@mui/material";
-import {Add, Settings} from "@mui/icons-material";
 import Navbar from "./components/Navbar";
 export function App() {
-    const ref = useRef(null);
-    const temp = "John";
-    const [toggled, setToggled] = React.useState(false);
-
     return (
         <div>
             <Navbar/>
