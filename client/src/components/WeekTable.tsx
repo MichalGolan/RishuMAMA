4aa293ac3a1014afc104c907a7a680dd287d6cc0
michalAronova <michalaronova@gmail.com>
2023-07-21 16:16:35 +0300

    changed demo-data to display "today appointments" which changes it from arbitrary date to date in current week

--- client/src/components/WeekTable.tsx
+++ client/src/components/WeekTable.tsx
@@ -6,13 +6,13 @@ import {
     Appointments,
 } from '@devexpress/dx-react-scheduler-material-ui';
 
-import {appointments} from '../demo-data/appointments';
+import appointments from '../demo-data/today-appointments';
 
 function WeekTable() {
     return (
         <Paper>
             <Scheduler data={appointments} height={660}>
-                <WeekView startDayHour={8} endDayHour={20} />
+                <WeekView startDayHour={8} endDayHour={20} excludedDays={[6]} />
                 <Appointments />
             </Scheduler>
         </Paper>
