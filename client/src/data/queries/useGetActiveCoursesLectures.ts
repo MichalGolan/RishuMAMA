656b852ae810dabbb0d029a2713f2b2ca7082e1a
michalAronova <michalaronova@gmail.com>
2023-08-30 13:03:28 +0300

    able to choose lectures by groups!

--- client/src/data/queries/useGetActiveCoursesLectures.ts
+++ client/src/data/queries/useGetActiveCoursesLectures.ts
@@ -1,40 +1,6 @@
 import {DBLectureToFELecture, getLectures, Lecture} from "../api/lectures";
 import {useQuery} from "@tanstack/react-query";
 
-// type Lecture = {
-//   id: string,
-//   courseId: number,
-//   start: Date,
-//   end: Date
-// }
-//
-// const coursesActiveLectures: Array<Lecture> = [{
-//
-//     id: '1',
-//     courseId: 1,
-//     start: new Date('2023-08-13T18:15:00'),
-//     end: new Date('2023-08-13T19:15:00'),
-//   },
-//   {
-//     id: '2',
-//     courseId: 1,
-//     start: new Date('2023-08-13T18:15:00'),
-//     end: new Date('2023-08-13T19:15:00'),
-//   },
-//   {
-//     id: '3',
-//     courseId: 2,
-//     start: new Date('2023-08-13T18:15:00'),
-//     end: new Date('2023-08-13T19:15:00'),
-//   },
-//   {
-//     id: '4',
-//     courseId: 2,
-//     start: new Date('2023-08-13T18:15:00'),
-//     end: new Date('2023-08-13T19:15:00'),
-//   },
-// ];
-
 export function useGetActiveCoursesLecturesQuery(ids: Array<number>) {
   return useQuery({
     queryKey: ['active-courses-lectures'],
@@ -43,5 +9,4 @@ export function useGetActiveCoursesLecturesQuery(ids: Array<number>) {
       return response.data.result?.map(DBLectureToFELecture);
     },
   })
-  //return coursesActiveLectures.filter((lecture) => ids.includes(lecture.courseId));
 }
