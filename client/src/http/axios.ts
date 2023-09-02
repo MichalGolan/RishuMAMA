c6f0e17670d91e61a1acca8c0d8cd7732df5c266
MichalGolan <michalgolan0107@gmail.com>
2023-09-02 14:12:03 +0300

    fix

--- client/src/http/axios.ts
+++ client/src/http/axios.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const baseURL = process.env.NODE_ENV === 'production' ? "https://api-production-740f.up.railway.app" : "localhost:3000";
+const baseURL = process.env.NODE_ENV === 'production' ? "https://api-production-740f.up.railway.app" : "http://localhost:3000";
+console.log(baseURL);
 export const httpClient = axios.create({
   baseURL,
 });
