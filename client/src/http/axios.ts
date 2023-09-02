024c76b821f8cda4dd59357759996824d0fcec70
MichalGolan <michalgolan0107@gmail.com>
2023-09-02 13:13:42 +0300

    api prod url

--- client/src/http/axios.ts
+++ client/src/http/axios.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
+const baseURL = process.env.NODE_ENV === 'production' ? "https://api-production-740f.up.railway.app" : "localhost:3000";
 export const httpClient = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL,
 });
