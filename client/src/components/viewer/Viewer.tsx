83a9d2455827622a77c98801d220c4ba5e0d069e
MichalGolan <michalgolan0107@gmail.com>
2023-09-09 18:25:04 +0300

    test deploy

--- client/src/components/viewer/Viewer.tsx
+++ client/src/components/viewer/Viewer.tsx
@@ -24,7 +24,7 @@ import {releaseAllColors, releaseColor, reserveAvailableColor} from "../../utils
 import {User} from "../../data/api/users";
 import ExamBoard from "../exam board/ExamBoard";
 
-import Logo from "../../../../client/assets/Logo.png";
+//import Logo from "../../../../client/assets/Logo.png";
 
 
 const StyledToolbar = styled(Toolbar)({
@@ -216,7 +216,7 @@ const Viewer = () => {
                 <StyledToolbar>
                     <SideBox>
                         <Typography variant="h6" noWrap sx={{ flexGrow: 1 }} component="div">
-                            <img src={Logo} alt="Your SVG" />
+                            {/* <img src={Logo} alt="Your SVG" /> */}
                         </Typography>
                         <Typography variant="h6" noWrap sx={{ flexGrow: 1 }} component="div">
                             Hello {isLoggedIn ? user.name : 'guest'}
