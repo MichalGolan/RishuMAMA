b76dcf2fe8f152397135bfa3282b35750cf58577
MichalGolan <michalgolan0107@gmail.com>
2023-09-11 11:55:57 +0300

    exam board title

--- client/src/components/viewer/Viewer.tsx
+++ client/src/components/viewer/Viewer.tsx
@@ -9,6 +9,7 @@ import Box from "@mui/material/Box";
 import Divider from "@mui/material/Divider";
 import Drawer from "@mui/material/Drawer";
 import IconButton from "@mui/material/IconButton";
+import IosShareIcon from '@mui/icons-material/IosShare';
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography"
 import {styled, useTheme} from "@mui/material/styles"
